18f646e74b1a3a933ed99bc96b7896441c64e50a
Vewaxio <49758078+vewaxio@users.noreply.github.com>
2026-01-03 19:25:23 +0700

    fix: improve media extraction and network reliability and bump version to 1.2.1

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -11,8 +11,8 @@ android {
         applicationId = "com.example.smulerod"
         minSdk = 24
         targetSdk = 34
-        versionCode = 13
-        versionName = "1.2.0"
+        versionCode = 14
+        versionName = "1.2.1"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
         vectorDrawables {
